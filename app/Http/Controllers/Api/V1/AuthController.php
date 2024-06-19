a657288680e53ee28c262f6fe42da78ddfd92e49
Sait Dellg16 <saitikiz@gmail.com>
2024-06-19 06:29:44 +0300

    dev6 end

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        $credentials = request(['email', 'password']);
+        if (!auth()->attempt($credentials)) {
+            return response()->json([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'password' => [
+                        'Invalid credentials'
+                    ],
+                ]
+            ], 422);
+        }
+
+        $user = User::where('email', $request->email)->first();
+        $authToken = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $authToken,
+        ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -12,10 +12,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use SoftDeletes, Notifiable, Auditable, HasFactory;
+    use SoftDeletes, Notifiable, Auditable, HasFactory,HasApiTokens;
 
     public $table = 'users';
 
